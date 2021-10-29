a72447461b315da850587cf39210f7e9f5fa8b4f
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-10-29 23:36:16 +0300

    add functionality to edit bulletin.

--- Controllers/BulletinController.cs
+++ Controllers/BulletinController.cs
@@ -75,5 +75,55 @@ namespace HalmakTechnologies.BulletinBoard
       }
       return View(bulletinList);
     }
+
+    public IActionResult Edit(int id)
+    {
+      string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+
+      Bulletin bulletin = new Bulletin();
+      using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+        using (NpgsqlCommand command = new NpgsqlCommand())
+        {
+          command.CommandText = "Select * From Bulletin Where BulletID='{id}'";
+
+          using(NpgsqlDataReader reader = command.ExecuteReader())
+          {
+            while(reader.Read())
+            {
+              //Bulletin bulletin = new Bulletin();
+              bulletin.BulletinID = Convert.ToInt32(reader["BulletinID"]);
+              bulletin.Subject = Convert.ToString(reader["Subject"]);
+              bulletin.Description = Convert.ToString(reader["Description"]);
+              bulletin.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+              bulletin.DateCreated = Convert.ToDateTime(reader["DateCreated"]);
+            }
+          }
+          connection.Close();
+        }
+        return View(bulletin);
+      }  
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Bulletin bulletin, int id)
+    {
+      string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+
+      using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+
+        using (NpgsqlCommand command = new NpgsqlCommand())
+        {
+          command.CommandText = "Edit Bulletin SET Subject='{bulletin.Subject}', Description='{bulletin.Description}' Where Id='{id}'";
+
+          command.ExecuteNonQuery();
+          connection.Close();
+        }
+      }
+      return RedirectToAction("Index");
+    }
   }
 }
