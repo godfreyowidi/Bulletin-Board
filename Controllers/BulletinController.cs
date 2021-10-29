769d4e8f052b13fec400d8171fe722c000cf4a54
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-10-29 23:10:02 +0300

    add functionality to read bulletin.

--- Controllers/BulletinController.cs
+++ Controllers/BulletinController.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
@@ -38,6 +40,40 @@ namespace HalmakTechnologies.BulletinBoard
         }
       }
       return View();
+    }
+    
+    public IActionResult Index()
+    {
+      List<Bulletin> bulletinList = new List<Bulletin>();
+
+      string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+      using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+
+        using (NpgsqlCommand command = new NpgsqlCommand())
+        {
+          command.CommandText = "Select * From Bulletin";
+          using(NpgsqlDataReader reader = command.ExecuteReader())
+          {
+            while(reader.Read())
+            {
+              Bulletin bulletin = new Bulletin();
+              bulletin.BulletinID = Convert.ToInt32(reader["BulletinID"]);
+              bulletin.Subject = Convert.ToString(reader["Subject"]);
+              bulletin.Description = Convert.ToString(reader["Description"]);
+              bulletin.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+              bulletin.DateCreated = Convert.ToDateTime(reader["DateCreated"]);
+              //string subject = reader.GetString(reader.GetOrdinal("Subject"));
+              //string description = reader.GetString(reader.GetOrdinal("Description"));
+
+              bulletinList.Add(bulletin);
+            }
+          }
+        }
+        connection.Close();
+      }
+      return View(bulletinList);
     }
   }
 }
